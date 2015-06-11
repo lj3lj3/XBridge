31cffaf46886c87f6773f7ea50d74642b4fdeddc
DayLemK <lj3lj3@gmail.com>
2015-06-11 18:07:07 +0800

    +add market-browser switch

--- app/src/main/java/daylemk/xposed/xbridge/action/PlayAction.java
+++ app/src/main/java/daylemk/xposed/xbridge/action/PlayAction.java
@@ -21,22 +21,28 @@ public class PlayAction extends Action {
     public static final String TAG = "PlayAction";
     public static final String STR_DESC = "View in Play Store";
     public static final String PKG_NAME = "com.android.vending";
+    public static final String URI_BROWSER = "http://play.google.com/store/apps/details?id=";
+    public static final String URI_MARKET = "market://details?id=";
 
     /* the key should the sub class overwrite ------------begin */
     public static String keyShowInStatusBar;
     public static String keyShowInRecentTask;
     public static String keyShowInAppInfo;
     public static String keyShow;
+    // add for market - browser switch
+    public static String keyIsMarket;
 
     public static boolean showInStatusBarDefault = true;
     public static boolean showInRecentTaskDefault = true;
     public static boolean showInAppInfoDefault = true;
     public static boolean showDefault = true;
+    public static boolean isMarketDefault = false;
 
     public static boolean isShowInRecentTask = true;
     public static boolean isShowInStatusBar = true;
     public static boolean isShowInAppInfo = true;
     public static boolean isShow = true;
+    public static boolean isMarket = false;
     /* the key should the sub class overwrite ------------end */
     // just need to init the icon and listener once
     // EDIT: maybe the icon is already one instance in the system
@@ -54,11 +60,13 @@ public class PlayAction extends Action {
         keyShowInAppInfo = sModRes.getString(R.string.key_play_app_info);
         keyShowInRecentTask = sModRes.getString(R.string.key_play_recent_task);
         keyShowInStatusBar = sModRes.getString(R.string.key_play_status_bar);
+        keyIsMarket = sModRes.getString(R.string.key_play_is_market);
         // get the default value of this action
         showInStatusBarDefault = sModRes.getBoolean(R.bool.play_status_bar_default);
         showInRecentTaskDefault = sModRes.getBoolean(R.bool.play_recent_task_default);
         showInAppInfoDefault = sModRes.getBoolean(R.bool.play_app_info_default);
         showDefault = sModRes.getBoolean(R.bool.play_default);
+        isMarketDefault = sModRes.getBoolean(R.bool.play_is_market_default);
     }
 
     public static void loadPreference(SharedPreferences preferences) {
@@ -70,9 +78,10 @@ public class PlayAction extends Action {
                 showInAppInfoDefault);
         isShow = preferences.getBoolean(keyShow,
                 showDefault);
+        isMarket = preferences.getBoolean(keyIsMarket, isMarketDefault);
         Log.d(TAG, "load preference: " + "isShowInStatusBar:" + isShowInStatusBar +
                 "isShowInRecentTask:" + isShowInRecentTask + "isShowInAppInfo:" + isShowInAppInfo
-                + "isShow:" + isShow);
+                + "isShow:" + isShow + "isMarket:" + isMarket);
     }
 
     public static boolean onReceiveNewValue(String key, String value) {
@@ -85,6 +94,8 @@ public class PlayAction extends Action {
             isShowInRecentTask = Boolean.valueOf(value);
         } else if (key.equals(keyShowInStatusBar)) {
             isShowInStatusBar = Boolean.valueOf(value);
+        } else if (key.equals(keyIsMarket)) {
+            isMarket = Boolean.valueOf(value);
         } else {
             // if not found it, return false
             result = false;
@@ -113,8 +124,7 @@ public class PlayAction extends Action {
     @Override
     protected Intent getIntent(Hook hook, Context context, String pkgName) {
         Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(Uri.parse("http://play.google.com/store/apps/details?id="
-                + pkgName));
+        intent.setData(Uri.parse((isMarket ? URI_MARKET : URI_BROWSER) + pkgName));
         return intent;
     }
 
